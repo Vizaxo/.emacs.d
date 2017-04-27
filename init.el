9668edd3e795feb56f82a80840d5f120bc7752f3
Vizaxo <vizaxo@mailfence.com>
2017-04-27 12:12:50 +0100

    Move custom-set-variables to setqs

--- init.el
+++ init.el
@@ -1,33 +1,38 @@
 (package-initialize)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(blink-cursor-mode nil)
- '(custom-enabled-themes (quote (deeper-blue)))
- '(menu-bar-mode nil)
- '(nyan-wavy-trail t)
- '(package-archives
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"]))
+
+(custom-set-faces
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 131 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
+
+;; General settings
+(setq blink-cursor-mode nil)
+(setq menu-bar-mode nil)
+(setq tool-bar-mode nil)
+(setq scroll-bar-mode nil)
+
+;; Theme
+(load-theme 'deeper-blue t)
+
+;; Packages
+(setq package-archives
    (quote
     (("melpa" . "https://stable.melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/"))))
- '(package-selected-packages (quote (magit helm org melpa-upstream-visit)))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+(setq package-selected-packages (quote (magit
+					org
+					melpa-upstream-visit)))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 131 :width normal :foundry "DAMA" :family "Ubuntu Mono")))))
+;; Ido fuzzy-search
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
 
+;; Org
 (setq org-refile-use-outline-path 'file)
 (setq org-directory "~/data/org")
 (setq org-agenda-files '("~/data/org"))
@@ -35,11 +40,12 @@
 (setq select-enable-primary t)
 (setf org-blank-before-new-entry '((heading . nil) (plain-list-item . nil)))
 
-;;Put all backup files into ~/tmp/backups
-(setq backup-directory-alist '(("." . "~/tmp/backups")))
-(setq backup-by-copying t)
-(setq org-default-notes-file (concat org-directory "/inbox.org"))
-
+;; Org-capture
 (setq org-capture-templates '(("a" "" entry (file "~/data/org/inbox.org") "")))
 (define-key global-map "\C-cc"
   (lambda () (interactive) (org-capture nil "a")))
+
+;; Put all backup files into ~/tmp/backups
+(setq backup-directory-alist '(("." . "~/tmp/backups")))
+(setq backup-by-copying t)
+(setq org-default-notes-file (concat org-directory "/inbox.org"))
