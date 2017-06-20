f773816c0e1b16dd55db13f772ab4b56d927f253
Vizaxo <vizaxo@mailfence.com>
2017-06-20 16:59:18 +0100

    Update TLS checking to cache the result on each machine

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ;; Properly configure TLS
-;; This will throw an error at startup if the connection isn't secure
+;; This will throw an error at startup if the connection isn't secure;
+;; But the result will be cached on each machine, so Emacs doesn't
+;; require an internet connection to start.
 ;; Requires `python`, `python -m certifi` and `gnutls-cli`
 ;; Install with:
 ;; python -m pip install --user certifi
@@ -7,46 +9,48 @@
 ;; dnf install gnutls-utils (Fedora)
 ;; Based on:
 ;; https://glyph.twistedmatrix.com/2015/11/editor-malware.html#fnref:4
-(setq tls-checktrust t)
-(let ((trustfile
-       (replace-regexp-in-string
-	"\\\\" "/"
-	(replace-regexp-in-string
-	 "\n" ""
-	 (shell-command-to-string "python -m certifi")))))
-  (setq tls-program
-	(list
-	 (format "gnutls-cli%s --x509cafile %s -p %%p %%h"
-		 (if (eq window-system 'w32) ".exe" "") trustfile))))
-
-(require 'cl)
-(let ((bad-hosts
-       (loop for bad
-	     in `("https://wrong.host.badssl.com/"
-		  "https://self-signed.badssl.com/")
-	     if (condition-case e
-		    (url-retrieve
-		     bad (lambda (retrieved) t))
-		  (error nil))
-	     collect bad)))
-  (if bad-hosts
-      (error (format (concat "TLS misconfigured; retrieved %s ok."
-			     "To fix:"
-			     "apt install python"
-			     "dnf install python"
-			     "python -m pip install --user certifi"
-			     "apt install gnutls-bin (Debian-based)"
-			     "dnf install gnutls-utils (Fedora)")
-		     bad-hosts))
-    (url-retrieve "https://badssl.com"
-		  (lambda (retrieved) t))))
+(let ((tls-safe (concat user-emacs-directory "tls-safe")))
+  (unless (file-exists-p tls-safe)
+    (setq tls-checktrust t)
+    (let ((trustfile
+           (replace-regexp-in-string
+            "\\\\" "/"
+            (replace-regexp-in-string
+             "\n" ""
+             (shell-command-to-string "python -m certifi")))))
+      (setq tls-program
+            (list
+             (format "gnutls-cli%s --x509cafile %s -p %%p %%h"
+                     (if (eq window-system 'w32) ".exe" "") trustfile))))
+    (require 'cl)
+    (let ((bad-hosts
+           (loop for bad
+                 in `("https://wrong.host.badssl.com/"
+                      "https://self-signed.badssl.com/")
+                 if (condition-case e
+                        (url-retrieve
+                         bad (lambda (retrieved) t))
+                      (error nil))
+                 collect bad)))
+      (cond (bad-hosts
+             (error (format (concat "TLS misconfigured; retrieved %s ok."
+                                    "To fix:"
+                                    "apt install python (Debian)"
+                                    "dnf install python (Fedora)"
+                                    "python -m pip install --user certifi"
+                                    "apt install gnutls-bin (Debian)"
+                                    "dnf install gnutls-utils (Fedora)")
+                            bad-hosts))
+             (url-retrieve "https://badssl.com"
+                           (lambda (retrieved))))
+            (t (with-temp-buffer (write-file tls-safe)))))))
 
 ;; Packages
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
-	("gnu"   . "https://elpa.gnu.org/packages/")))
+        ("gnu"   . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 
