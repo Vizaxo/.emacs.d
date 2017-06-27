78bbb0edd7999daaa3f009f119dc2282f33fafad
Vizaxo <vizaxo@mailfence.com>
2017-06-27 12:49:16 +0100

    Convert init.el to Org mode literate programming

---
+++ package-setup.el
@@ -0,0 +1,8 @@
+;; Basic package setup
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu"   . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
